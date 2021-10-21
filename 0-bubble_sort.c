f8cb0ec6c995877c25d3171954afb1a26b4cbb57
SandraCalero <3358@holbertonschool.com>
2021-10-21 12:01:13 -0500

    add header file and a function that sorts an array of integers in ascending order using the Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: array to be sorted
+ * @size:
+ *
+ * Return: Always 0
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t element, i;
+	int temp;
+
+	/*Number of times the array is cycled through*/
+	for (element = 0 ; element < (size - 1); element++)
+	{
+		/*Swap Iterator*/
+		for (i = 0 ; i < (size - 1) ; i++)
+		{
+			/*Ask if current number is greater than the following*/
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
